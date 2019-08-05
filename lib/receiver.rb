968390d2d29d8f8386ebc650f4af2532c3e5b0fe
saypop <tome.m.dj@gmail.com>
2019-08-05 19:54:32 +0100

    52nd test passing: - Class: Receiver; - Method: initialize; - Test: contains a hash of acceptable yes/no inputs.

--- lib/receiver.rb
+++ lib/receiver.rb
@@ -1,12 +1,15 @@
 class Receiver
 
-  attr_reader :pre_menu_options, :main_menu_options
+  attr_reader :pre_menu_options, :main_menu_options, :yes_no_options
 
   def initialize
     @pre_menu_options = { 'exit' => :exit, 'start' => :start, 'back' => :exit }
     @main_menu_options = {
       1 => :one, 2 => :two, 3 => :three, 4 => :four,
-      5 => :five, 0 => :exit, 'exit' => :exit
+      5 => :five, 0 => :exit, 'exit' => :exit, 'back' => :exit
+    }
+    @yes_no_options = {
+      'yes' => :yes, 'no' => :no, 'back' => :back, 'exit' => :exit
     }
   end
 
@@ -16,6 +19,10 @@ class Receiver
 
   def main_menu_choice(selection)
     @main_menu_options[selection]
+  end
+
+  def yes_no_choice(selection)
+    @yes_no_options[selection]
   end
 
 end
--- spec/receiver_spec.rb
+++ spec/receiver_spec.rb
@@ -13,7 +13,13 @@ describe Receiver do
     it 'contains a list of acceptable main menu options' do
       expect(receiver.main_menu_options).to eq({
         1 => :one, 2 => :two, 3 => :three, 4 => :four,
-        5 => :five, 0 => :exit, 'exit' => :exit
+        5 => :five, 0 => :exit, 'exit' => :exit, 'back' => :exit
+      })
+    end
+
+    it 'contains a list of acceptable yes/no menu options' do
+      expect(receiver.yes_no_options).to eq({
+        'yes' => :yes, 'no' => :no, 'back' => :back, 'exit' => :exit
       })
     end
   end
@@ -43,6 +49,34 @@ describe Receiver do
 
     it 'returns :one when given 1 as an option' do
       expect(receiver.main_menu_choice(1)).to eq :one
+    end
+
+    it 'returns :two when given 2 as an option' do
+      expect(receiver.main_menu_choice(2)).to eq :two
+    end
+
+    it 'returns :three when given 3 as an option' do
+      expect(receiver.main_menu_choice(3)).to eq :three
+    end
+
+    it 'returns :four when given 4 as an option' do
+      expect(receiver.main_menu_choice(4)).to eq :four
+    end
+
+    it 'returns :five when given 5 as an option' do
+      expect(receiver.main_menu_choice(5)).to eq :five
+    end
+
+    it 'returns :exit when given 0 as an option' do
+      expect(receiver.main_menu_choice(0)).to eq :exit
+    end
+
+    it 'returns :exit when given exit as an option' do
+      expect(receiver.main_menu_choice('exit')).to eq :exit
+    end
+
+    it 'returns :exit when given back as an option' do
+      expect(receiver.main_menu_choice('back')).to eq :exit
     end
 
   end
