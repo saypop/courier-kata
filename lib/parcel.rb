bfb061c3d894f1269c6d826bec4785171efd7100
saypop <tome.m.dj@gmail.com>
2019-08-05 21:29:15 +0100

    61st test passing: - Class: Parcel; - Method: initialize; - Test: it has default properties.

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,8 +1,10 @@
 class Parcel
 
+  DEFAULT_NAME, DEFAULT_LENGTH, DEFAULT_WIDTH, DEFAULT_HEIGHT = 'Unnamed Parcel', 1, 1, 1
+
   attr_reader :name, :length, :width, :height
 
-  def initialize(name, length, width, height)
+  def initialize(name=DEFAULT_NAME, length=DEFAULT_LENGTH, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT)
     @name = name
     @length = length
     @width = width
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -2,6 +2,7 @@ require 'parcel'
 
 describe Parcel do
   let(:brown_box) {described_class.new('Brown Box', 40, 20, 25)}
+  let(:unknown_box) {described_class.new}
 
   describe '#initialize' do
     it 'has a name property' do
@@ -19,5 +20,13 @@ describe Parcel do
     it 'has a height property' do
       expect(brown_box.height).to eq 25
     end
+
+    it 'has default properties' do
+      expect(unknown_box.name).to eq 'Unnamed Parcel'
+      expect(unknown_box.length).to eq 1
+      expect(unknown_box.width).to eq 1
+      expect(unknown_box.height).to eq 1
+    end
+
   end
 end
