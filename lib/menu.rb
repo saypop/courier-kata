aa45428d863834617e57c0084bd328a1c43ec8d1
saypop <tome.m.dj@gmail.com>
2019-08-05 15:43:49 +0100

    20th passing test: - Class: Menu; - Method: welcome_message; - Test: returns the welcome string.

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,8 +4,21 @@ class Menu
 
   def initialize
     @options = {
-      :welcome => "Hello world"
+      :welcome => "
+      Hello, world.
+      I am the FundApps courier service bot.
+      You can use me to get the cost of sending parcels.
+      I'll provide you with numbered options as we go along but you can talk to me in English too.
+      Whenever I give you numbered options just type in the number and press enter.
+      If you want to give me instructions in English just type the instruction without quotation marks and press enter.
+      If you'd like to turn me off at anytime just type and enter: 'end'.
+      To get started please type and enter: 'start'
+      "
     }
+  end
+
+  def welcome_message
+    @options[:welcome]
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,7 +7,21 @@ describe Menu do
     it 'contains a hash with labelled instructions for each step of the app' do
       expect(menu.options).to be_a(Hash)
     end
+  end
 
+  describe '#welcome_message' do
+    it 'returns a welcome message' do
+      expect(menu.welcome_message.delete(' ')).to eq(
+        "Hello, world.
+        I am the FundApps courier service bot.
+        You can use me to get the cost of sending parcels.
+        I'll provide you with numbered options as we go along but you can talk to me in English too.
+        Whenever I give you numbered options just type in the number and press enter.
+        If you want to give me instructions in English just type the instruction without quotation marks and press enter.
+        If you'd like to turn me off at anytime just type and enter: 'end'.
+        To get started please type and enter: 'start'".delete(' ')
+      )
+    end
   end
 
 end
