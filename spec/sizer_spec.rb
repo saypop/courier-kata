eef6aea762d0b33129d87fef08272a46967180ab
saypop <tome.m.dj@gmail.com>
2019-08-05 13:56:06 +0100

    11th passing test: - Class: Sizer; - Method: categorise; - Test: Return large for large parcel.

--- spec/sizer_spec.rb
+++ spec/sizer_spec.rb
@@ -16,8 +16,13 @@ describe Sizer do
     end
 
     it 'returns a medium category for medium parcels' do
-      medium_box = double('parcel', name: 'Medium Box', length: 40, width: 20, height: 35)
+      medium_box = double('parcel', name: 'Medium Box', length: 40, width: 20, height: 5)
       expect(sizer.categorise(medium_box)).to eq 'Medium'
+    end
+
+    it 'returns a large category for large parcels' do
+      large_box = double('parcel', name: 'Large Box', length: 90, width: 45, height: 5)
+      expect(sizer.categorise(large_box)).to eq 'Large'
     end
   end
 
