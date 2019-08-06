789a56e5040072830a5c27f9ebd6eaf47dc54f33
saypop <tome.m.dj@gmail.com>
2019-08-06 09:43:44 +0100

    80th test passing: - Class: Printer; - Method: puts_output; - Test: puts an output to STDOUT.

--- lib/printer.rb
+++ lib/printer.rb
@@ -3,5 +3,9 @@ class Printer
   def print_output(output)
     print(output)
   end
+  
+  def puts_output(output)
+    puts(output)
+  end
 
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -5,8 +5,13 @@ describe Printer do
 
   describe '#print_output' do
     it 'prints an output to the terminal' do
-      expect{ printer.print_output('This is a test') }.to output('This is a test').to_stdout
+      expect{ printer.print_output('This is a test') }.to output("This is a test").to_stdout
     end
   end
 
+  describe '#puts_output' do
+    it 'puts an output to the terminal' do
+      expect{ printer.puts_output('This is a test') }.to output("This is a test\n").to_stdout
+    end
+  end
 end
